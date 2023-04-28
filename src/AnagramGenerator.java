a5b46ad3bc1601027a99b024e105a12f4b78bcda
Christian Johnson <cartian4@gmail.com>
2023-04-28 06:12:37 -0400

    Compiled class

--- src/AnagramGenerator.java
+++ src/AnagramGenerator.java
@@ -3,9 +3,11 @@ public class AnagramGenerator {
 
     public static void main (String[] args) {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter word: ");
-
+        System.out.print("Enter word: ");
         String inputWord = myObj.nextLine();
-        System.out.println("Word is: " + inputWord);
+
+
+
+
     }
 }
