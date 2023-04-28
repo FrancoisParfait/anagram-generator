4e53b0a7f496d1b9e59292a840c3adf439f22955
Christian Johnson <cartian4@gmail.com>
2023-04-27 21:47:41 -0400

    Add new main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter word: ");
+
+        String inputWord = myObj.nextLine();
+        System.out.println("Word is: " + inputWord);
+    }
 }
