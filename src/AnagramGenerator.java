09548fcd8d9a4ef5b18f5d54a23e2b712c45fac9
Christian Johnson <cartian4@gmail.com>
2023-05-01 06:20:09 -0400

    Remove WordListHash class, move to one class

--- src/AnagramGenerator.java
+++ src/AnagramGenerator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashSet;
 public class AnagramGenerator {
 
     public static void main(String[] args) {
--- src/WordListHash.java
+++
@@ -1,3 +0,0 @@
-public class WordListHash extends AnagramGenerator {
-
-}
