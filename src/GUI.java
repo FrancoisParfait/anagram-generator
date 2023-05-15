1e9e01aad4a71181a4b52c90337d04a0bd4bf52d
Christian Johnson <cartian4@gmail.com>
2023-05-15 05:24:15 -0400

    Add label to GUI

--- src/GUI.java
+++ src/GUI.java
@@ -3,11 +3,20 @@ import javax.swing.*;
 public class GUI extends JFrame {
 
     GUI() {
+
+        ImageIcon image = new ImageIcon("anagram-2.png");
+
+        JLabel label = new JLabel();
+        label.setText("Let's make some anagrams");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+
         this.setTitle("Anagram Generator");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(640, 640);
         this.setResizable(false);
         this.setVisible(true);
+        this.add(label);
     }
 
 }
