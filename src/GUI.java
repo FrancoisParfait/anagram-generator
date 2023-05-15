8646f9ffad13aea06eacdc045f573f91ec3352b1
Christian Johnson <cartian4@gmail.com>
2023-05-15 05:58:57 -0400

    Finish label

--- src/GUI.java
+++ src/GUI.java
@@ -6,12 +6,17 @@ public class GUI extends JFrame {
     GUI() {
 
         ImageIcon image = new ImageIcon("anagram-2.png");
+        Image image2 = image.getImage();
+        Image newImg = image2.getScaledInstance(160, 160, Image.SCALE_SMOOTH);
+        image = new ImageIcon(newImg);
 
         JLabel label = new JLabel();
         label.setText("Let's make some anagrams");
         label.setIcon(image);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.TOP);
         label.setFont(new Font("Z003", Font.PLAIN, 30));
 
         this.setTitle("Anagram Generator");
